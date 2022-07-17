5feaf4df55a6d3a1a0e988b022c88c9c22a30da0
vishruthsk.eth <59907367+vishruthsk@users.noreply.github.com>
2022-07-17 12:09:05 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,15 +7,15 @@ const { DAI, DAI_WHALE, POOL_ADDRESS_PROVIDER } = require("../config");
 
 describe("Deploy a Flash Loan", function () {
   it("Should take a flash loan and be able to return it", async function () {
-    const flashLoanExample = await ethers.getContractFactory(
-      "FlashLoanExample"
+    const flashLoan = await ethers.getContractFactory(
+      "flashloan"
     );
 
-    const _flashLoanExample = await flashLoanExample.deploy(
+    const _flashloan = await flashloan.deploy(
       // Address of the PoolAddressProvider: you can find it here: https://docs.aave.com/developers/deployed-contracts/v3-mainnet/polygon
       POOL_ADDRESS_PROVIDER
     );
-    await _flashLoanExample.deployed();
+    await _flashloan.deployed();
 
     const token = await ethers.getContractAt("IERC20", DAI);
     const BALANCE_AMOUNT_DAI = ethers.utils.parseEther("2000");
@@ -28,11 +28,11 @@ describe("Deploy a Flash Loan", function () {
     const signer = await ethers.getSigner(DAI_WHALE);
     await token
       .connect(signer)
-      .transfer(_flashLoanExample.address, BALANCE_AMOUNT_DAI); // Sends our contract 2000 DAI from the DAI_WHALE
+      .transfer(_flashloan.address, BALANCE_AMOUNT_DAI); // Sends our contract 2000 DAI from the DAI_WHALE
 
-    const tx = await _flashLoanExample.createFlashLoan(DAI, 1000); // Borrow 1000 DAI in a Flash Loan with no upfront collateral
+    const tx = await _flashloan.createFlashLoan(DAI, 1000); // Borrow 1000 DAI in a Flash Loan with no upfront collateral
     await tx.wait();
-    const remainingBalance = await token.balanceOf(_flashLoanExample.address); // Check the balance of DAI in the Flash Loan contract afterwards
+    const remainingBalance = await token.balanceOf(_flashloan.address); // Check the balance of DAI in the Flash Loan contract afterwards
     expect(remainingBalance.lt(BALANCE_AMOUNT_DAI)).to.be.true; // We must have less than 2000 DAI now, since the premium was paid from our contract's balance
   });
 });
