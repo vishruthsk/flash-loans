3fc3ac6b5e299e054c3928a81f3be7ca4ef7759d
vishruthsk.eth <59907367+vishruthsk@users.noreply.github.com>
2022-07-17 12:07:17 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { expect, assert } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers, waffle, artifacts } = require("hardhat");
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const { DAI, DAI_WHALE, POOL_ADDRESS_PROVIDER } = require("../config");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+describe("Deploy a Flash Loan", function () {
+  it("Should take a flash loan and be able to return it", async function () {
+    const flashLoanExample = await ethers.getContractFactory(
+      "FlashLoanExample"
+    );
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const _flashLoanExample = await flashLoanExample.deploy(
+      // Address of the PoolAddressProvider: you can find it here: https://docs.aave.com/developers/deployed-contracts/v3-mainnet/polygon
+      POOL_ADDRESS_PROVIDER
+    );
+    await _flashLoanExample.deployed();
 
-  await lock.deployed();
+    const token = await ethers.getContractAt("IERC20", DAI);
+    const BALANCE_AMOUNT_DAI = ethers.utils.parseEther("2000");
+    
+    // Impersonate the DAI_WHALE account to be able to send transactions from that account
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [DAI_WHALE],
+    });
+    const signer = await ethers.getSigner(DAI_WHALE);
+    await token
+      .connect(signer)
+      .transfer(_flashLoanExample.address, BALANCE_AMOUNT_DAI); // Sends our contract 2000 DAI from the DAI_WHALE
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    const tx = await _flashLoanExample.createFlashLoan(DAI, 1000); // Borrow 1000 DAI in a Flash Loan with no upfront collateral
+    await tx.wait();
+    const remainingBalance = await token.balanceOf(_flashLoanExample.address); // Check the balance of DAI in the Flash Loan contract afterwards
+    expect(remainingBalance.lt(BALANCE_AMOUNT_DAI)).to.be.true; // We must have less than 2000 DAI now, since the premium was paid from our contract's balance
+  });
 });
